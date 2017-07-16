6eb874f67cd52d0196deb5f3fda7ed89e5a03149
Daniel Grinberg <daniel@daniels-mbp.home>
2017-07-16 11:54:49 +0300

    done

---
+++ v1_embedding/indexer_tester.py
@@ -0,0 +1,12 @@
+from v1_embedding.word_indexing import WordIndexer
+import os
+
+# pathname = os.path.join("..", "data", 'text8.zip')
+# text8indexing = WordIndexer(pathname)
+# # print(text8indexing.sample_indices())
+# print(text8indexing.word2ind('dfg'))
+# print(text8indexing.word2ind('abuse'))
+
+# filename = os.path.join("..", "datasets", 'yelp', 'negative_reviews.json')
+# yelpidx = WordIndexer(filename)
+# print(yelpidx.sample_indices())
