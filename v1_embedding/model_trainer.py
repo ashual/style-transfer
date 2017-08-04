2f79f113a07981e9a87ecb01a1e056a06fd56eb7
tomjur <tomjurgenson@gmail.com>
2017-08-04 12:41:41 +0300

    v1 - start of gan script2

--- v1_embedding/model_trainer.py
+++ v1_embedding/model_trainer.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+import time
+
+from datasets.multi_batch_iterator import MultiBatchIterator
 from datasets.yelp_helpers import YelpSentences
 from v1_embedding.embedding_translator import EmbeddingTranslator
 from v1_embedding.embedding_encoder import EmbeddingEncoder
@@ -32,9 +35,10 @@ class ModelTrainer(ModelTrainerBase):
                                          dataset_cache_dir=self.dataset_cache_dir, dataset_name='neg')
         self.dataset_pos = YelpSentences(positive=True, limit_sentences=self.config['sentence']['limit'],
                                          dataset_cache_dir=self.dataset_cache_dir, dataset_name='pos')
+        datasets = [self.dataset_neg, self.dataset_pos]
         self.embedding_handler = WordIndexingEmbeddingHandler(
             self.embedding_dir,
-            [self.dataset_neg, self.dataset_pos],
+            datasets,
             self.config['embedding']['word_size'],
             self.config['embedding']['min_word_occurrences']
         )
@@ -53,30 +57,33 @@ class ModelTrainer(ModelTrainerBase):
         self.loss_handler = LossHandler()
 
         # losses:
-        self.target_adversarial_loss = self.get_discriminator_loss(self.left_padded_source_batch,
-                                                                   self.left_padded_target_batch,
-                                                                   self.right_padded_target_batch,
-                                                                   self.target_identifier)
-        self.source_adversarial_loss = self.get_discriminator_loss(self.left_padded_target_batch,
-                                                                   self.left_padded_source_batch,
-                                                                   self.right_padded_source_batch,
-                                                                   self.source_identifier)
+        target_adversarial_loss = self.get_discriminator_loss(self.left_padded_source_batch,
+                                                              self.left_padded_target_batch,
+                                                              self.right_padded_target_batch,
+                                                              self.target_identifier)
+        source_adversarial_loss = self.get_discriminator_loss(self.left_padded_target_batch,
+                                                              self.left_padded_source_batch,
+                                                              self.right_padded_source_batch,
+                                                              self.source_identifier)
+        self.adversarial_loss = target_adversarial_loss + source_adversarial_loss
 
-        self.target_generator_loss = self.get_generator_loss(self.left_padded_source_batch,
-                                                             self.right_padded_source_batch,
-                                                             self.left_padded_target_batch,
-                                                             self.right_padded_target_batch,
+        target_generator_loss = self.get_generator_loss(self.left_padded_source_batch,
+                                                        self.right_padded_source_batch,
+                                                        self.left_padded_target_batch,
+                                                        self.right_padded_target_batch,
                                                              self.target_identifier)
-        self.source_generator_loss = self.get_generator_loss(self.left_padded_target_batch,
-                                                             self.right_padded_target_batch,
-                                                             self.left_padded_source_batch,
-                                                             self.right_padded_source_batch,
-                                                             self.source_identifier)
+        source_generator_loss = self.get_generator_loss(self.left_padded_target_batch,
+                                                        self.right_padded_target_batch,
+                                                        self.left_padded_source_batch,
+                                                        self.right_padded_source_batch,
+                                                        self.source_identifier)
+        self.generator_loss = target_generator_loss + source_generator_loss
+
         # train steps
         discriminator_optimizer = tf.train.GradientDescentOptimizer(self.config['model']['learn_rate'])
         discriminator_var_list = self.discriminator.get_trainable_parameters()
         discriminator_grads_and_vars = discriminator_optimizer.compute_gradients(
-            self.source_adversarial_loss + self.target_adversarial_loss,
+            self.adversarial_loss,
             colocate_gradients_with_ops=True, var_list=discriminator_var_list
         )
         self.discriminator_train_step = discriminator_optimizer.apply_gradients(discriminator_grads_and_vars)
@@ -85,10 +92,23 @@ class ModelTrainer(ModelTrainerBase):
         generator_var_list = self.encoder.get_trainable_parameters() + self.decoder.get_trainable_parameters() + \
                              self.embedding_translator.get_trainable_parameters()
         generator_grads_and_vars = generator_optimizer.compute_gradients(
-            self.target_generator_loss + self.source_generator_loss,
+            self.generator_loss,
             colocate_gradients_with_ops=True, var_list=generator_var_list
         )
         self.generator_train_step = generator_optimizer.apply_gradients(generator_grads_and_vars)
+
+        # iterators
+        self.batch_iterator = MultiBatchIterator(datasets, self.embedding_handler,
+                                                 self.config['sentence']['min_length'],
+                                                 self.config['model']['batch_size'])
+
+        # iterators
+        self.validation_batch_iterator = MultiBatchIterator(datasets, self.embedding_handler,
+                                                            self.config['sentence']['min_length'],
+                                                            1000)
+        # train loop parameters:
+        self.best_loss = float('inf')
+        self.generator_mode = True
 
     @staticmethod
     def _get_discriminator_index(identifier):
@@ -169,3 +189,52 @@ class ModelTrainer(ModelTrainerBase):
         return self.config['reconstruction_coefficient'] * reconstruction_loss \
                + self.config['semantic_distance_coefficient'] * semantic_distance_loss \
                + anti_d_loss
+
+    def do_before_train_loop(self, sess):
+        sess.run(self.embedding_translator.assign_embedding(), {
+            self.embedding_translator.embedding_placeholder: self.embedding_handler.embedding_np
+        })
+
+    def do_train_batch(self, sess, global_step, epoch_num, batch_index, batch):
+        if self.generator_mode:
+            feed_dict = {
+                self.left_padded_source_batch: batch[0].left_padded_sentences,
+                self.left_padded_target_batch: batch[1].left_padded_sentences,
+                self.right_padded_source_batch: batch[0].right_padded_sentences,
+                self.right_padded_target_batch: batch[1].right_padded_sentences,
+                self.dropout_placeholder: self.config['model']['dropout'],
+                self.discriminator_dropout_placeholder: self.config['model']['discriminator_dropout'],
+                self.encoder.should_print: self.operational_config['debug'],
+                self.decoder.should_print: self.operational_config['debug'],
+            }
+            # TODO: outputs to measure progress, summaries
+            execution_list = [self.generator_train_step, self.generator_loss]
+            # print results
+            if batch_index % 100 == 0:
+                # start_time = time.time()
+                # _, generator_loss = sess.run(execution_list, feed_dict)
+                # total_time = time.time() - start_time
+                # self.print_side_by_side(batch.right_padded_sentences, decoded_output, batch.right_padded_masks)
+                # print('epoch-index: {} batch-index: {} acc: {} loss: {} runtime: {}'.format(epoch_num, batch_index,
+                #                                                                             batch_acc, loss_output,
+                #                                                                             total_time))
+                print()
+            else:
+                # will not run summaries
+                _, loss_output, decoded_output, batch_acc = sess.run(execution_list[:-1], feed_dict)
+            # TODO: train source->target and target->source
+        else:
+            # should train discriminator
+            print()
+
+    def do_validation_batch(self, sess, global_step, epoch_num, batch_index, batch):
+        pass
+
+    def do_after_train_loop(self, sess):
+        pass
+
+    def do_before_epoch(self, sess):
+        pass
+
+    def do_after_epoch(self, sess):
+        pass
