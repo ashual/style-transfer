21c7b973cc2df8ce80270eda7a1ba8d871b92e1d
tomjur <tomjurgenson@gmail.com>
2017-08-19 11:50:51 +0300

    other margin loss

--- v1_embedding/loss_handler.py
+++ v1_embedding/loss_handler.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 from v1_embedding.base_model import BaseModel
-from tensorflow.contrib.losses import sigmoid_cross_entropy
 
 
 class LossHandler(BaseModel):
@@ -23,9 +22,6 @@ class LossHandler(BaseModel):
         mask_sum = tf.reduce_sum(mask)
         return sum / mask_sum
 
-    def mean_squared_error(self, source, target):
-        return tf.losses.mean_squared_error(source, target)
-
     def get_margin_loss(self, target, padding_mask, random_words, margin):
         len_random_words = tf.shape(random_words)[2]
         target_expand = tf.expand_dims(target, 2)
@@ -41,14 +37,27 @@ class LossHandler(BaseModel):
         mask_sum = tf.reduce_sum(mask)
         return sum / mask_sum
 
-    def cosine_distance(self, encoded_source, encoded_dest, batch_size, sentence_length, embedding_size):
-        print(encoded_source.shape)
-        print(encoded_dest.shape)
-        endoded_reshape = tf.reshape(encoded_source, (batch_size * (sentence_length - 1), embedding_size))
-        endoded_dest_reshape = tf.reshape(encoded_dest, (batch_size * (sentence_length - 1), embedding_size))
-        print(endoded_dest_reshape.shape)
-        print(endoded_reshape.shape)
-        return tf.losses.cosine_distance(endoded_reshape, endoded_dest_reshape, 1)
+    def get_margin_loss_v2(self, true_embeddings, decoded_embeddings, random_words_embeddings, padding_mask, margin):
+        len_random_words = tf.shape(random_words_embeddings)[2]
+
+        sq_difference = tf.sqrt(tf.reduce_sum(tf.squared_difference(true_embeddings, decoded_embeddings), axis=-1))
+        sq_difference_expand = tf.expand_dims(sq_difference, 2)
+        sq_difference_expand = tf.tile(sq_difference_expand, [1, 1, len_random_words])
+
+        target_expand = tf.expand_dims(decoded_embeddings, 2)
+        target_expand = tf.tile(target_expand, [1, 1, len_random_words, 1])
+        per_random_word_distance = tf.sqrt(
+        tf.reduce_sum(tf.squared_difference(target_expand, random_words_embeddings), axis=-1))
+
+        per_word_margin_loss = tf.maximum(0.0, margin + sq_difference_expand - per_random_word_distance)
+        per_word_distance = tf.reduce_mean(per_word_margin_loss, axis=-1)
+
+        mask = tf.where(padding_mask, tf.ones_like(padding_mask, dtype=tf.float32),
+                        tf.zeros_like(padding_mask, dtype=tf.float32))
+        sum = tf.reduce_sum(per_word_distance * mask)
+        mask_sum = tf.reduce_sum(mask)
+
+        return sum / mask_sum
 
     def get_sentence_reconstruction_loss(self, labels, logits):
         with tf.variable_scope('SentenceReconstructionLoss'):
