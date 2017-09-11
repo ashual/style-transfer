742bda2a3899316d646f39bb778d76af5d9df5d9
OA <ashualon@gmail.com>
2017-09-11 23:48:49 +0300

    Adding small 200-2 embedding

--- data/download.sh
+++ data/download.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 pip install -U textblob
 python -m textblob.download_corpora lite
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
+git lfs install
+git lfs fetch
+git lfs checkout
