b6783becc3f02f6c718215c0cae839737c07d76b
tomjur <tomjurgenson@gmail.com>
2017-10-28 19:28:41 +0300

    tf counter assign add

--- v1_embedding/tf_counter.py
+++ v1_embedding/tf_counter.py
@@ -4,5 +4,4 @@ import tensorflow as tf
 class TfCounter:
     def __init__(self, name):
         self.count = tf.Variable(0, dtype=tf.int32, trainable=False, name="{}_counter".format(name))
-        new_value = tf.add(self.count, tf.constant(1))
-        self.update = tf.assign(self.count, new_value)
+        self.update = tf.assign_add(self.count, 1)
