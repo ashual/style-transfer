cf0297281f78ce1aa114e6fc26d1a1716610b4c6
OA <ashualon@gmail.com>
2017-11-14 10:23:03 +0200

    Adding active passive datasets

--- datasets/activepassive/detector3.py
+++
@@ -1,7 +0,0 @@
-from datasets.activepassive.ispassive import Tagger
-
-t = Tagger()
-print(t.is_passive('Mistakes were made.'))
-
-print(t.is_passive('I made mistakes'))
-print(t.is_passive('adbfadfb'))
