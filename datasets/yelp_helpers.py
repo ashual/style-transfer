3b2e55c3ea5d0a93b910e05ed4cfe68c9570195d
OA <ashualon@gmail.com>
2017-11-25 10:39:02 +0200

    return the old parameters

--- datasets/yelp_helpers.py
+++ datasets/yelp_helpers.py
@@ -9,9 +9,9 @@ class YelpSentences(Dataset):
 
     def get_content_actual(self):
         if self.positive:
-            with open('datasets/yelp/regina-data/sentiment.train.1') as yelp:
+            with open('datasets/yelp/pos.txt') as yelp:
                 content = yelp.readlines()
         else:
-            with open('datasets/yelp/regina-data/sentiment.train.0') as yelp:
+            with open('datasets/yelp/neg.txt') as yelp:
                 content = yelp.readlines()
         return content
